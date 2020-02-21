db2199bed9fb71f7292e3a21027865c425f8f194
Artin <beyatiartin@gmail.com>
2020-02-21 23:11:45 +0100

    Work on car class

--- src/App.java
+++ src/App.java
@@ -1,2 +1,7 @@
 public class App {
+    public static void main(String[] args) {
+        Car car1 = new Car("A2B2C3", "Toyora", "Class B", "Stationwagon");
+
+        System.out.println(car1.carModel);
+    }
 }
--- src/Car.java
+++ src/Car.java
@@ -4,4 +4,14 @@ public class Car {
     String carModel = null;
     String carType = null;
     Boolean availability = false;
+
+     public Car(String licensePlate, String carBrand, String carModel, String carType) {
+        this.licensePlate = licensePlate;
+        this.carBrand = carBrand;
+        this.carModel = carModel;
+        this.carType = carType;
+        availability = true;
+
+        System.out.println("The car " + carBrand + ", with license plate: " + licensePlate + " is now registered.");
+     }
 }
